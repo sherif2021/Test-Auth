43b1a3c3553d70613829a8d1c439e906065dc4a5
gala <ajory.it@gmail.com>
2022-05-14 01:30:22 +0200

    sherif

--- lib/app/modules/auth_home/views/auth_home_view.dart
+++ lib/app/modules/auth_home/views/auth_home_view.dart
@@ -9,122 +9,117 @@ class AuthHomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 30,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Image.asset(
+              'assets/images/logo.jpg',
+              height: 150,
+              width: 150,
+            ),
+            const Text(
+              'Welcome Back!',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 22,
               ),
-              Image.asset(
-                'assets/images/logo.jpg',
-                height: 150,
-                width: 150,
-              ),
-              const Text(
-                'Welcome Back!',
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 22,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Let\'s Share your talent with the world',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            OutlinedButton(
+              onPressed: () => Get.toNamed(Routes.LOGIN),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 75),
+                child: Text(
+                  'Sign in with Email',
+                  style: Get.theme.textTheme.bodyMedium,
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Let\'s Share your talent with the world',
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              OutlinedButton(
-                onPressed: () => Get.toNamed(Routes.LOGIN),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 75),
-                  child: Text(
-                    'Sign in with Email',
-                    style: Get.theme.textTheme.bodyMedium,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Expanded(
+                    child: Divider(
+                      thickness: .7,
+                    ),
                   ),
-                ),
+                  Text('OR'),
+                  Expanded(
+                    child: Divider(
+                      thickness: .7,
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 75),
+              child: OutlinedButton(
+                onPressed: () {},
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Expanded(
-                      child: Divider(
-                        thickness: .7,
-                      ),
-                    ),
-                    Text('OR'),
-                    Expanded(
-                      child: Divider(
-                        thickness: .7,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
                   children: [
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/google.png',
-                            height: 20,
-                            width: 20,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            'Sign up with Google',
-                            style: Get.theme.textTheme.bodyMedium,
-                          ),
-                        ],
-                      ),
+                    Image.asset(
+                      'assets/images/google.png',
+                      height: 20,
+                      width: 20,
                     ),
                     const SizedBox(
-                      width: 10,
+                      width: 5,
                     ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/facebook.png',
-                            height: 20,
-                            width: 20,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            'Sign up with Facebook',
-                            style: Get.theme.textTheme.bodyMedium,
-                          ),
-                        ],
-                      ),
+                    Text(
+                      'Sign up with Google',
+                      style: Get.theme.textTheme.bodyMedium,
                     ),
                   ],
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Don\'t have an account? '),
-                  TextButton(
-                      onPressed: () => Get.toNamed(Routes.REGISTER),
-                      child: const Text('Sign up')),
-                ],
-              )
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 75),
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/facebook.png',
+                      height: 20,
+                      width: 20,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Sign up with Facebook',
+                      style: Get.theme.textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Don\'t have an account? '),
+                TextButton(
+                    onPressed: () => Get.toNamed(Routes.REGISTER),
+                    child: const Text('Sign up')),
+              ],
+            )
+          ],
         ),
       ),
     );
